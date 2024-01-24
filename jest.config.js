e9d24c6659e408f984a6ff2fb6fc0568d58db902
patiljayant <contactjaypatil@gmail.com>
2024-01-25 01:55:05 +0530

    init

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,5 @@
 module.exports = {
-  collectCoverageFrom: ["src/**/*.js", "!**/node_modules/**"],
-  coverageThreshold: {
-    global: {
-      branches: 80,
-      functions: 90,
-      lines: 90,
-      statements: 90,
-    },
-    "./src/utils": {
-      branches: 100,
-      functions: 100,
-      lines: 100,
-      statements: 100,
-    },
-  },
+  collectCoverage: true,
+  collectCoverageFrom: ['**/*.{js}', '!**/node_modules/**'],
+  coverageReporters: ['text', 'text-summary'],
 };
