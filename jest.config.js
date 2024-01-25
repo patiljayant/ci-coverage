901c801d846c146dcfaad4d867c9a0a1ef551512
patiljayant <contactjaypatil@gmail.com>
2024-01-25 18:35:14 +0530

    tets

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   collectCoverage: true,
-  collectCoverageFrom: ['**/*.{js}', '!**/node_modules/**'],
-  coverageReporters: ['text', 'text-summary'],
+  collectCoverageFrom: ["src/**/*.js", '!**/node_modules/**']
+  // coverageReporters: ['text', 'text-summary'],
 };
