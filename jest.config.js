681b41415fdac3a6e57ef5e2af21fc36a290f0f7
patiljayant <contactjaypatil@gmail.com>
2024-01-25 18:44:50 +0530

    test

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,4 @@
 module.exports = {
   collectCoverage: true,
-  collectCoverageFrom: ['**/*.{js}', '!**/node_modules/**'],
-  coverageReporters: ['text', 'text-summary'],
+  collectCoverageFrom: ["src/**/*.js", '!**/node_modules/**']
 };
